749d6a16d1087d828fea04453ca7942a816e8d0d
hm-shaug <shaug@hm.edu>
2023-04-22 18:28:29 +0200

    put project in repository https://github.com/Project-Delta-37/CaculatorTutorialApp.git

---
+++ CalculatorTutorial/app/src/main/java/com/growth/calculatortutorial/MainActivity.java
@@ -0,0 +1,80 @@
+package com.growth.calculatortutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText number1EditText, number2EditText;
+    TextView resultTextView;
+    Button addButton, subtractButton, multiplyButton, divideButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        number1EditText = findViewById(R.id.number1EditText);
+        number2EditText = findViewById(R.id.number2EditText);
+        resultTextView = findViewById(R.id.resultTextView);
+        addButton = findViewById(R.id.addButton);
+        subtractButton = findViewById(R.id.subtractButton);
+        multiplyButton = findViewById(R.id.multiplyButton);
+        divideButton = findViewById(R.id.divideButton);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate('+');
+            }
+        });
+
+        subtractButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate('-');
+            }
+        });
+
+        multiplyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate('*');
+            }
+        });
+
+        divideButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate('/');
+            }
+        });
+    }
+
+    private void calculate(char operator) {
+        double num1 = Double.parseDouble(number1EditText.getText().toString());
+        double num2 = Double.parseDouble(number2EditText.getText().toString());
+        double result = 0;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+        }
+
+        resultTextView.setText(String.valueOf(result));
+    }
+}
